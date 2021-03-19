87e53edbfe27d631716bec12c26d5f9b5fc30169
VeraBohatyrenko <bogatyrenkovera@gmail.com>
2021-03-19 23:50:27 +0200

    Добавляет решение задач с 20 - 25 ( 3 - модуль)

--- js/module-3.js
+++ js/module-3.js
@@ -403,3 +403,145 @@
 // console.log(getAllPropValues('n'));
 
 /* Задача №-20 */
+
+// Напиши функцию calculateTotalPrice(productName) которая принимает один параметр productName - название товара. Функция должна вернуть общую стоимость (цена * количество) товара с таким именем из массива products.
+
+// const products = [
+//   { name: 'Радар', price: 1300, quantity: 4 },
+//   { name: 'Сканер', price: 2700, quantity: 3 },
+//   { name: 'Дроид', price: 400, quantity: 7 },
+//   { name: 'Захват', price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   // Пиши код ниже этой строки
+// for (const product of products) {
+//   if (product.name === productName) {
+//     return product.name = product.price * product.quantity;
+//   }
+// }
+// return 0;
+//   // Пиши код выше этой строки
+// }
+
+// /* Задача №-21 */
+
+// Пришел трёхдневный прогноз максимальных температур и мы считаем среднюю температуру за три дня(meanTemperature).Замени объявления переменных yesterday, today и tomorrow одной операцией деструктуризации свойств объекта highTemperatures.
+
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Пиши код ниже этой строки
+// const {yesterday, today, tomorrow} = highTemperatures;
+// // Пиши код выше этой строки
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+
+// /* Задача №-22 */
+// В прогнозе максимальных температур также может быть необязательное свойство icon - иконка погоды.Замени объявления переменных yesterday, today, tomorrow и icon одной операцией деструктуризации свойств объекта highTemperatures.Задай значение по умолчанию для icon - строку 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg'.
+
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Пиши код ниже этой строки
+// const {
+//   yesterday, 
+//   today, 
+//   tomorrow, 
+//   icon='https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+// } = highTemperatures;
+
+// // Пиши код выше этой строки
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+
+// /* Задача №-23 */
+
+// Замени объявления переменных highYesterday, highToday, highTomorrow и highIcon одной операцией деструктуризации свойств объекта highTemperatures.Задай значение по умолчанию для highIcon - строку 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg'.
+
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Пиши код ниже этой строки
+// const {
+//  yesterday: highYesterday,
+//  today: highToday,
+//  tomorrow: highTomorrow,
+//  icon: highIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg' 
+// } = highTemperatures;
+
+// // Пиши код выше этой строки
+// const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+
+
+// /* Задача №-24 */
+
+// Выполни рефакторинг цикла for...of так, чтобы в нём использовалась деструктуризация объекта..
+
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Пиши код ниже этой строки
+
+// for (const {hex, rgb} of colors) {
+//   hexColors.push(hex);
+//   rgbColors.push(rgb);
+// }
+
+// /* Задача №-25 */
+
+// Мы получили прогноз погоды на два дня, с минимальными и максимальными температурами, а также необязательными иконками. Замени объявления всех переменных одной операцией деструктуризации свойств объекта forecast. Задай значение по умолчанию для иконок, переменных todayIcon и tomorrowIcon - строку 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg'.
+
+// const forecast = {
+//   today: {
+//     low: 28,
+//     high: 32,
+//     icon: 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg',
+//   },
+//   tomorrow: {
+//     low: 27,
+//     high: 31,
+//   },
+// };
+// // Пиши код ниже этой строки
+
+// const { 
+//   today: { low: lowToday, high: highToday, icon: todayIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg' },
+//   tomorrow: { low: lowTomorrow, high: highTomorrow, icon: tomorrowIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg' },
+// } = forecast;
+
+// /* Задача №-26 */
+
+// Пиши код ниже этой строки
+calculateMeanTemperatureForecast({
+today: {
+ low: 10,
+ high: 20,
+}, 
+tomorrow: { 
+ low: 20, 
+ high: 30  
+},
+})
+function calculateMeanTemperature(forecast) {
+  const { today: { low: todayLow, high: todayHigh } 
+  tomorrow: { low: tomorrowLow, high: tomorrowHigh } 
+  } = forecast;
+ 
+ console.log(forecast);
+
+  // Пиши код выше этой строки
+  return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+}
